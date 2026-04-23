e0858b2096fdd5d06f4afce390e1670774dd82cd
raafi <raafistudent@gmail.com>
2026-04-23 17:36:37 +0700

    Mengatur routing web untuk menghubungkan halaman utama dengan controller

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\FloodController;
 use Illuminate\Support\Facades\Route;
 
+// Halaman Landing Page Utama
 Route::get('/', [FloodController::class, 'index'])->name('home');
 
+// Route bawaan Breeze (jika kamu ingin menggunakan dashboard admin nantinya)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
