86f9d2aa3948c616fe91a2b6429bb5f1bbdf8862
raafi <raafistudent@gmail.com>
2026-04-23 21:38:33 +0700

    Membuat model Threshold untuk mengatur ambang batas ketinggian air

--- app/Models/Threshold.php
+++ app/Models/Threshold.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Threshold extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['batas_siaga', 'batas_bahaya'];
 }
