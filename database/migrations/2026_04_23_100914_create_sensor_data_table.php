534e4e2b7c8e1b38261d2320f3742af9abe60809
raafi <raafistudent@gmail.com>
2026-04-23 17:29:01 +0700

    Membuat migration SensorData untuk menyimpan data ketinggian air dan status banjir

--- database/migrations/2026_04_23_100914_create_sensor_data_table.php
+++ database/migrations/2026_04_23_100914_create_sensor_data_table.php
@@ -6,24 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
-{
-    Schema::create('sensor_data', function (Blueprint $table) {
-        $table->id();
-        $table->float('water_level'); // Data sensor JSN-SR04T [cite: 221]
-        $table->string('rain_status'); // Data sensor hujan [cite: 222]
-        $table->float('water_flow');  // Data sensor waterflow [cite: 223]
-        $table->string('status');      // Aman, Siaga, Bahaya [cite: 449]
-        $table->timestamps();
-    });
-}
+    {
+        Schema::create('sensor_data', function (Blueprint $table) {
+            $table->id();
+            $table->float('water_level'); // Level air (cm)
+            $table->string('rain_status')->nullable(); // Hujan/Tidak
+            $table->float('water_flow')->nullable();  // Kecepatan aliran
+            $table->string('status'); // AMAN, SIAGA, BAHAYA
+            $table->timestamps();
+        });
+    }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('sensor_data');
