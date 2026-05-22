0e1f556bd058fe3e8b3ecd474517349220b520bb
raafi <raafistudent@gmail.com>
2026-05-22 14:47:53 +0700

    feast: menambahkan isentitas hujan

--- database/migrations/2026_05_22_074633_add_rain_to_sensor_data_table.php
+++ database/migrations/2026_05_22_074633_add_rain_to_sensor_data_table.php
@@ -6,23 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('sensor_data', function (Blueprint $table) {
-            //
+            // Menambahkan 2 kolom baru setelah kolom status
+            $table->float('rain_value')->default(0)->after('status'); 
+            $table->string('rain_status')->default('CERAH')->after('rain_value');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('sensor_data', function (Blueprint $table) {
-            //
+            $table->dropColumn(['rain_value', 'rain_status']);
         });
     }
 };
