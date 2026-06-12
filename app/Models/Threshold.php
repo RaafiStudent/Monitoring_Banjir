a8505c8c8cef9fe7de0ae5a731a9153f1ec472e6
raafi <raafistudent@gmail.com>
2026-06-12 19:49:22 +0700

    feast: manambahkan wasapada di model treshold

--- app/Models/Threshold.php
+++ app/Models/Threshold.php
@@ -8,5 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Threshold extends Model
 {
     use HasFactory;
-    protected $fillable = ['batas_siaga', 'batas_bahaya'];
+    // Tambahkan batas_waspada
+    protected $fillable = ['batas_waspada', 'batas_siaga', 'batas_bahaya'];
 }
